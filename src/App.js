b58d0cb37724818fc3d9801af4d26bc05f39a7ee
kajal nikam <kajalnikam29@gmail.com>
2024-06-10 14:19:43 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SignUpForm from "./SignUpForm";
 import Login from "./Login";
 
 function App() {
   return (
-    <BrowserRouter> 
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignUpForm />} />
+
+        <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
